422fcc7a4caf4d3ac410e1c3d9612f938eae7600
danielleleigh <danielle.leigh90@gmail.com>
2021-12-07 16:20:34 -0600

    add import and began coponent for Buyer Page. Add imports for NavBar

---
+++ src/components/BuyerPage/buyerPage.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const BuyerPage = (props) => {
+  return <div></div>;
+};
+
+export default BuyerPage;
--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 const NavBar = ({ user }) => {
   return (
     <div>
@@ -31,3 +34,5 @@ const NavBar = ({ user }) => {
     </div>
   );
 };
+
+export default NavBar;
