111fb1dc7f7789ca7fb10951c3874480ea4a8318
sonya brazell <92110758+sonyabrazell@users.noreply.github.com>
2021-12-07 16:55:14 -0600

    Small text change on Seller page.

--- src/components/SellerPage/sellerPage.jsx
+++ src/components/SellerPage/sellerPage.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 const SellerPage = (props) => {
-  return (
-    <h1>This is our Buyer Page.</h1>
-  )
-  };
+    return (
+    <h1>This is our Seller Page.</h1>
+)
+};
 
 export default SellerPage;
