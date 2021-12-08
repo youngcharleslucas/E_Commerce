654c7c399b82b6d6994e2eaadf1552b736eaca24
sonya brazell <92110758+sonyabrazell@users.noreply.github.com>
2021-12-08 16:47:18 -0600

    Add Hero Image and Buttons to HomePage.jsx.

--- src/components/HomePage/homePage.jsx
+++ src/components/HomePage/homePage.jsx
@@ -1,6 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { Image } from 'react-bootstrap';
+
 const HomePage = (props) => {
     return ( 
-        <h1>This is our Home Page.</h1>
+
+        <div>
+            <Container>
+                <Image src="https://unsplash.com/photos/m-HDOBfSI7I" fluid/>
+                <Link to="/buyer">
+                    <Button variant="dark" size="lg">Buy</Button>
+                </Link>
+                <Link to="/seller">
+                    <Button variant="dark" size="lg">Sell</Button>
+                </Link>
+            </Container>
+        </div>
+
     );
 }
 
