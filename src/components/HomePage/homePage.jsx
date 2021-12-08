8e3734d286bfa5f357917a164223e973f279ca52
sonya brazell <92110758+sonyabrazell@users.noreply.github.com>
2021-12-08 17:03:21 -0600

    End of day commit. Home page loading, must fix image link.

--- src/components/HomePage/homePage.jsx
+++ src/components/HomePage/homePage.jsx
@@ -4,18 +4,24 @@ import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import { Image } from 'react-bootstrap';
 
+
+
+
 const HomePage = (props) => {
     return ( 
 
         <div>
             <Container>
+                <React.Fragment>
                 <Image src="https://unsplash.com/photos/m-HDOBfSI7I" fluid/>
+                <h1>Handmade furniture, just for you.</h1>
                 <Link to="/buyer">
                     <Button variant="dark" size="lg">Buy</Button>
                 </Link>
                 <Link to="/seller">
                     <Button variant="dark" size="lg">Sell</Button>
                 </Link>
+                </React.Fragment>
             </Container>
         </div>
 
