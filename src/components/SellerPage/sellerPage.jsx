464e6a899689c8fede71d977edeb2f37bd1c9caa
danielleleigh <danielle.leigh90@gmail.com>
2021-12-09 13:44:26 -0600

    update layout of footer

--- src/components/SellerPage/sellerPage.jsx
+++ src/components/SellerPage/sellerPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./sellerPage.css";
 
 const SellerPage = (props) => {
-    return (
-        <h1>This is our Seller Page.</h1>
-    )
+  return (
+    <div align="center" className="seller-style">
+      <h1>This is our Seller Page.</h1>
+    </div>
+  );
 };
 
 export default SellerPage;
