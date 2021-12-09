b406f46e6d1d4e57fa8b019fafff8d7ea97d2aeb
sonya brazell <92110758+sonyabrazell@users.noreply.github.com>
2021-12-09 15:52:57 -0600

    Add initial search bar and styling on buyerPage.jsx.

--- src/components/BuyerPage/buyerPage.jsx
+++ src/components/BuyerPage/buyerPage.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Card, Container, ListGroup , ListGroupItem, CardGroup } from "react-bootstrap";
+import { Card, Container, ListGroup , ListGroupItem, CardGroup, Form, FormControl, Button } from "react-bootstrap";
 import './buyerPage.css'
 import axios from "axios";
 
 const BuyerPage = (props) => {
   const [products, setProducts] = useState([])
- 
   
     useEffect(()=>{
       getProduct()
@@ -13,15 +12,24 @@ const BuyerPage = (props) => {
 
     const getProduct = async () => {
       let response = await axios.get('https://localhost:44394/api/products')
-     setProducts(response.data)
+      setProducts(response.data)
     
   }
-  
+
 
   return (
     <Container fluid>
       <React.Fragment>
         {console.log(products)}
+          <Form className="d-flex" className="product-style">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+        <Button variant="dark">Search</Button>
+      </Form>
         <CardGroup style={{display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
           {products.map((element)=>
           <Card style={{flex: 1, width: '20px'}}>
