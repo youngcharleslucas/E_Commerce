42e4ca61a793ac8cd22ca1eb64aa0d7e9c1a6e3f
danielleleigh <danielle.leigh90@gmail.com>
2021-12-13 14:26:48 -0600

    change style of login page

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,4 +1,5 @@
 import React, { useState, Component } from "react";
+import { Form, Button, Container, FormLabel } from "react-bootstrap";
 import axios from "axios";
 import { Route } from "react-router-dom";
 import "./login.css";
@@ -25,26 +26,44 @@ const Login = (props) => {
 
   return (
     <div align="center" className="login-style">
-      <h2> Login </h2>
-      <form onSubmit={onFormSubmit}>
-        <input
-          type="text"
-          name="userName"
-          placeholder="User Name"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />{" "}
-        <br />
-        <input
-          type="text"
-          name="userPassword"
-          placeholder="Password"
-          value={userPassword}
-          onChange={(e) => setUserPassword(e.target.value)}
-        />{" "}
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+      <Container style={{ flex: 1, width: "40%", padding: "10%" }}>
+        <FormLabel
+          style={{
+            color: "white",
+            fontSize: "24px",
+            backgroundColor: "black",
+          }}
+        >
+          Login
+        </FormLabel>
+        <Form id="contact-form" onSubmit={onFormSubmit}>
+          <Form.Group controlId="formBasicFirstName">
+            <Form.Control
+              type="text"
+              name="userName"
+              placeholder="User Name"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </Form.Group>
+          &nbsp;
+          <Form.Group controlId="formBasicLastName">
+            <Form.Control
+              type="text"
+              name="userPassword"
+              placeholder="Password"
+              value={userPassword}
+              onChange={(e) => setUserPassword(e.target.value)}
+            />
+            &nbsp;
+          </Form.Group>
+          &nbsp;
+          <Button type="submit" variant="dark">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+      &nbsp;
     </div>
   );
 };
